44752920b3ef752ad8df91572b0ab5ea7ad0afcc
senseiwells <wellsm488@gmail.com>
2024-03-01 15:14:42 +0000

    Barebones example

--- src/main.rs
+++ src/main.rs
@@ -1,95 +1,50 @@
 use iced::executor;
-use iced::font::{self, Font};
-use iced::theme;
-use iced::widget::{checkbox, column, container, row, text};
+use iced::widget::{button, column, container, row, text};
 use iced::{Application, Command, Element, Length, Settings, Theme};
 
-const ICON_FONT: Font = Font::with_name("icons");
-
 pub fn main() -> iced::Result {
-    Example::run(Settings::default())
+    TubeTagApp::run(Settings::default())
 }
 
 #[derive(Default)]
-struct Example {
-    default: bool,
-    styled: bool,
-    custom: bool,
+struct TubeTagApp {
+    clicks : u32
 }
 
 #[derive(Debug, Clone, Copy)]
 enum Message {
-    DefaultToggled(bool),
-    CustomToggled(bool),
-    StyledToggled(bool),
-    FontLoaded(Result<(), font::Error>),
+    BtnClicked
 }
 
-impl Application for Example {
+impl Application for TubeTagApp {
     type Executor = executor::Default;
     type Message = Message;
     type Theme = Theme;
     type Flags = ();
 
     fn new(_flags: Self::Flags) -> (Self, Command<Message>) {
-        (
-            Self::default(),
-            font::load(include_bytes!("../fonts/icons.ttf").as_slice())
-                .map(Message::FontLoaded),
-        )
+        (Self::default(), Command::none())
     }
 
     fn title(&self) -> String {
-        String::from("Checkbox - Iced")
+        String::from("TubeTag")
     }
 
     fn update(&mut self, message: Message) -> Command<Message> {
         match message {
-            Message::DefaultToggled(default) => {
-                self.default = default;
+            Message::BtnClicked => {
+                self.clicks += 1;
             }
-            Message::StyledToggled(styled) => {
-                self.styled = styled;
-            }
-            Message::CustomToggled(custom) => {
-                self.custom = custom;
-            }
-            Message::FontLoaded(_) => (),
         }
 
         Command::none()
     }
 
     fn view(&self) -> Element<Message> {
-        let default_checkbox = checkbox("Default", self.default)
-            .on_toggle(Message::DefaultToggled);
-
-        let styled_checkbox = |label, style| {
-            checkbox(label, self.styled)
-                .on_toggle_maybe(self.default.then_some(Message::StyledToggled))
-                .style(style)
-        };
-
-        let checkboxes = row![
-            styled_checkbox("Primary", theme::Checkbox::Primary),
-            styled_checkbox("Secondary", theme::Checkbox::Secondary),
-            styled_checkbox("Success", theme::Checkbox::Success),
-            styled_checkbox("Danger", theme::Checkbox::Danger),
-        ]
-            .spacing(20);
-
-        let custom_checkbox = checkbox("Custom", self.custom)
-            .on_toggle(Message::CustomToggled)
-            .icon(checkbox::Icon {
-                font: ICON_FONT,
-                code_point: '\u{e901}',
-                size: None,
-                line_height: text::LineHeight::Relative(1.0),
-                shaping: text::Shaping::Basic,
-            });
-
-        let content =
-            column![default_checkbox, checkboxes, custom_checkbox].spacing(20);
+        let content = column!(
+            button("Click Me!").on_press(Message::BtnClicked),
+            text(self.clicks).size(50)
+        );
 
         container(content)
             .width(Length::Fill)
