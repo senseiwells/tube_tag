f4e9d51d74bb7a6fd8a5372e251cfdac964e7411
senseiwells <wellsm488@gmail.com>
2024-03-01 15:42:18 +0000

    Try getting panning and zooming to work

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
-use iced::executor;
-use iced::widget::{button, column, container, row, text};
+use std::fmt::format;
+use std::io::Read;
+use iced::{executor, theme};
+use iced::widget::{button, column, container, row, text, svg, image};
 use iced::{Application, Command, Element, Length, Settings, Theme};
+use iced::widget::image::viewer;
 
 pub fn main() -> iced::Result {
     TubeTagApp::run(Settings::default())
@@ -41,14 +44,33 @@ impl Application for TubeTagApp {
     }
 
     fn view(&self) -> Element<Message> {
-        let content = column!(
-            button("Click Me!").on_press(Message::BtnClicked),
-            text(self.clicks).size(50)
+        let map_path = format!(
+            "{}/assets/standard-tube-map-tube-only.png",
+            env!("CARGO_MANIFEST_DIR")
         );
 
-        container(content)
+        let map_handle = image::Handle::from_path(map_path);
+
+        let map_viewer = viewer(map_handle);
+
+        // Construct svg object from handle
+        // let tube_img = svg(tube_img_handle).width(Length::Fill).height(Length::Fill).style(
+        //     theme::Svg::Default
+        // );
+
+        let column_layout = column![
+            map_viewer
+        ];
+
+
+
+        container(
+            column_layout
+            .height(Length::Fill),
+        )
             .width(Length::Fill)
             .height(Length::Fill)
+            .padding(20)
             .center_x()
             .center_y()
             .into()
